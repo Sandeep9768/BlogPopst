e3db288ef3385354aafd5dea6142ae63bc9707c9
Sandeep <singhsandeeprt24@gmail.com>
2020-07-30 04:24:44 +0530

    First commit

--- constants/navigations.js
+++
@@ -1,5 +0,0 @@
-export const STARTUP_SCREEN = 'START_SCREEN';
-export const MAIN_SCREEN = 'Main';
-export const SETTINGS_SCREEN = 'Settings';
-export const TIMERS_SCREEN = 'TIMERS';
-export const TIMER_SCREEN = 'TIMER';
